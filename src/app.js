9e63294c1dab05e346e175ca8037000ae541de94
spoudel26 <sujanapoudel811@gmail.com>
2026-01-27 21:11:22 -0500

    Messy formatting for CI fail

--- src/app.js
+++ src/app.js
@@ -1,18 +1,1 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "Hello from CSP451" });
-});
-
-// BROKEN /health endpoint
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "healthy", uptime: process.uptime() }); // remove closing brace to break
-// MISSING CLOSING BRACE AND PARENTHESIS
-
-module.exports     = app;
-
-if (require.mai   n === module) {
-  const port      = process.env.PORT || 3000;
-  app       .listen(port, () => console.log(`Listening on ${port}`));
-}
+const express=require("express");const app=express();app.get("/",(req,res)=>{res.json({status:"ok",message:"Hello from CSP451"})});app.get("/health",(req,res)=>{res.status(200).json({status:"healthy",uptime:process.uptime()})});module.exports=app;if(require.main===module){const port=process.env.PORT||3000;app.listen(port,()=>console.log(`Listening on ${port}`))}
